bee33d60db11ab756f4ff173393e211462b3ca48
Александр Цветков <sickuenser@gmail.com>
2013-10-08 15:44:39 +0400

    Fixed mumamo bug

--- init.el
+++ init.el
@@ -353,4 +353,12 @@ See URL `http://pypi.python.org/pypi/flake8'."
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
+;; Workaround the annoying warnings:
+;;    Warning (mumamo-per-buffer-local-vars):
+;;    Already 'permanent-local t: buffer-file-name
+(when (equal emacs-major-version 24)
+  (eval-after-load "mumamo"
+    '(setq mumamo-per-buffer-local-vars
+           (delq 'buffer-file-name mumamo-per-buffer-local-vars))))
+
 (load-theme 'solarized-dark)
